83def6e7926d07ce29c55e7a12550181143595b1
RizaBasheer <rizabasheer10@gmail.com>
2024-11-28 08:47:18 +0530

    ok

--- login.php
+++ login.php
@@ -1,11 +1,15 @@
 <?php
-$name = $_POST['name'];
+$name = $_POST['uname'];
 $password = $_POST['pswd'];
-$db = pg_connect();
+$db = pg_connect("host=localhost port=5433 dbname=postgres user=postgres password=devagiri");
+echo" $name";
+$rs =pg_query($db, "SELECT * FROM login_page WHERE username = '$name' AND password = '$password'") ;
 
-if(!db){
-    die("Database connection failed: " preg_last_error());
+$count = pg_num_rows($rs);
+
+if($count == 1){
+    echo"<h1 align='center'>Welcome, $name !</h1>";
+}else{
+    echo"<h1 align='center'>Incorrect username or password.</h1>";
 }
-
-$query = "SELECT * FROM login WHERE username = $1 AND password = $2";
-$result = pg_prepare($db,"login_query,$query");
+?>
